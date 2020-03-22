84573dc147e236c0e0cac3c4b3b0ed360195a5e5
ziofat <printlts@gmail.com>
2020-03-22 19:22:05 +0800

    update example app to kotlin and swift

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=D:\flutter"
-export "FLUTTER_APPLICATION_PATH=D:\workspace\material_design_icons_flutter\example"
+export "FLUTTER_APPLICATION_PATH=D:\workspace\opensource\material_design_icons_flutter\example"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build\ios"
