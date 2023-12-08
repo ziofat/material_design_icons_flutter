a882d3327f9a6164d14dd15940740ab98157ff36
Eduardo Braga <eduardo@dinerama.com.br>
2023-12-08 18:47:55 +0000

    remove unneeded change from a file

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/eduardo/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/eduardo/material_design_icons_flutter/example"
+export "FLUTTER_ROOT=/Users/ziofatli/Documents/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/ziofatli/Documents/workspace/opensource/material_design_icons_flutter/example"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
